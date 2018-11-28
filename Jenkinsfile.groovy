8713223323a0718fc0a4d224163f971251ef1e30
Nathan Sullivan <nathan@pc-doctor.com>
2018-11-28 13:11:56 -0800

    start of squash commits, debugging jenkinsfile. commented out python

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,23 @@
+node{
+	stage('Build')
+	{
+		docker.image('python:2-alpine')
+		steps
+		{
+			echo "Im working HERE"
+			checkout([$class: 'GitSCM', \
+				branches: [[name: '*/jenkinsBranch']], \
+				doGenerateSubmoduleConfigurations: false, \
+				extensions: [], \
+				submoduleCfg: [], \
+				userRemoteConfigs: \
+				[[credentialsId: 'github', \
+				url: 'https://github.com/nathanPCD/creating-a-pipeline-in-blue-ocean/']]])
+		
+
+			//sh 'python -m py_compile sources/add2vals.py sources/calc.py' 
+
+				echo "done!!"
+		}
+	}
+}
